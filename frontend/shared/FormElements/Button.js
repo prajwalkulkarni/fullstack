329611dd37b29b75dc47bf39a43c846ca5c6ed89
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-11-11 22:29:30 +0530

    Custom button

---
+++ frontend/shared/FormElements/Button.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Link  from 'next/link';
+
+import styles from './Button.module.css';
+const Button = props => {
+  if (props.href) {
+    return (
+      <a
+        className={[styles[`button button--${props.size || 'default'}`], styles[`${props.inverse &&
+          'button--inverse'}`], styles[`${props.danger && 'button--danger'}`]].join(' ')}
+        href={props.href}
+      >
+        {props.children}
+      </a>
+    );
+  }
+  if (props.to) {
+    return (
+      <button className={[styles[`button`],styles[` button--${props.size || 'default'}`], styles[`${props.inverse &&
+        'button--inverse'}`] ,styles[`${props.danger && 'button--danger'}`]].join(' ')}>
+      
+        {props.children}
+    
+      </button>
+    );
+  }
+  return (
+    <button
+      className={[styles[`button`], styles[`button--${props.size || 'default'}`], styles[`${props.inverse &&
+        'button--inverse'}`], styles[`${props.danger && 'button--danger'}`]].join(' ')}
+      type={props.type}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
