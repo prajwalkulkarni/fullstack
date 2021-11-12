ab33303ce3f8cb207d5d651711f162e293123459
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-11-12 22:39:13 +0530

    Use express validator to validate inputs

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const app = express()
+const {check} = require('express-validator')
 
 
 const router = express.Router();
@@ -12,6 +13,14 @@ router.get('/:placeId',placeController.getByPlaceId)
 
 router.get('/user/:uid',placeController.getByUserId)
 
-router.post('/',placeController.createPlace)
+router.post('/',[
+    check('title').not().isEmpty(),
+    check('description').isLength({min:10}),
+    check('address').not().isEmpty()
+],placeController.createPlace)
+
+router.patch('/:pid',placeController.updatePlace)
+
+router.delete('/:pid',placeController.deletePlace)
 
 module.exports = router
