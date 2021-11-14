fc03d4bfd38207757e7783e573c68540cc200f0a
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-11-14 23:39:46 +0530

    Add delete handler modal

--- frontend/places/components/PlaceItem.js
+++ frontend/places/components/PlaceItem.js
@@ -3,15 +3,32 @@ import styles from './PlaceItem.module.css'
 import React, { useState } from "react"
 import Modal from '../../shared/UIElements/Modal'
 import Button from "../../shared/FormElements/Button"
+import Map from "../../shared/UIElements/Map"
+import Head from 'next/head'
+
 export default function PlaceItem(props){
 
     const [showMap,setShowMap] = useState(false)
+    const [showConfirmModal,setShowConfirmModal] = useState(false)
 
     const openMapHandler = () => setShowMap(true)
     const closeMapHandler = () => setShowMap(false)
+
+    const showDeleteWarningHandler = () => setShowConfirmModal(true)
+    const cancelDeleteHandler  = () => setShowConfirmModal(false)
+
+    const confirmDeleteHandler = () =>{
+        setShowConfirmModal(false)
+        console.log("Delete")
+    }
     return(
 
         <React.Fragment>
+            <Head>
+            <script
+    src="https://maps.googleapis.com/maps/api/js?key=API_KEY" async defer>
+</script>
+            </Head>
             <Modal 
                 show={showMap}
                 onCancel={closeMapHandler}
@@ -21,8 +38,25 @@ export default function PlaceItem(props){
                 footer={<Button onClick={closeMapHandler}>CLOSE</Button>}>
 
                 <div className={styles['map-container']}>
-                    <h3>GOOGLE MAPS</h3>
+                    <Map center={props.coordinates} zoom={10}/>
                 </div>
+            </Modal>
+            <Modal 
+                show={showConfirmModal}
+                header="Are you sure"
+                contentClass='place-item__modal-content'
+                footerClass='place-item__modal-actions'
+                footer={
+                    <React.Fragment>
+                        <Button inverse onClick={cancelDeleteHandler}>Cancel</Button>
+                        <Button danger onClick={confirmDeleteHandler}>Delete</Button>
+                    </React.Fragment>
+                }>
+
+                <p>
+                    Are you sure you'd want to delete this item?
+                    Note that, this action can't be undone thereafter.
+                </p>
             </Modal>
 
         <li key={props.id} className={styles['place-item']}>
@@ -39,7 +73,7 @@ export default function PlaceItem(props){
                 <div className={styles['place-item__actions']}>
                     <Button onClick={openMapHandler} inverse>View on map</Button>
                     <Button to={`/places/${props.id}`}>Edit</Button>
-                    <Button danger>Delete</Button>
+                    <Button danger onClick={showDeleteWarningHandler}>Delete</Button>
                 </div>
             </Card>
         </li>
