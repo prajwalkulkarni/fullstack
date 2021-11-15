b4a8572ebebcf82daed3ecf8cd10e103f4234f72
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-11-15 22:29:31 +0530

    Destructure setform data

--- frontend/places/components/UpdatePlace.js
+++ frontend/places/components/UpdatePlace.js
@@ -1,10 +1,28 @@
 import Button from "../../shared/FormElements/Button"
 import Input from "../../shared/FormElements/Input"
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/utils/validators"
-
+import { useForm } from "../../shared/hooks/form-hook"
 export default function UpdatePlace(props){
+    // console.log(props.placeToBeUpdated)
 
+    // const {title} = props.placeToBeUpdated
+    const {formState,inputHandler,setFormData} =useForm({
+        title:{
+            value:props.placeToBeUpdated.title,
+            isValid:true
+        },
+        description:{
+            value:props.placeToBeUpdated.description,
+            isValid:true
+        }
+    },true)
+
+    const placeUpdateSubmitHandler = event=>{
+        event.preventDefault()
+
+    }
     if(!props.placeToBeUpdated){
+        console.log("Invoked")
         return(
             <div>
                 <h1>Place not found</h1>
@@ -13,15 +31,16 @@ export default function UpdatePlace(props){
     }
 
     return (
-        <form>
+        <form onSubmit={placeUpdateSubmitHandler}>
             <Input id="title"
             element="input"
             type="text"
             label="Title"
             validators={[VALIDATOR_REQUIRE()]}
             errorText="Please enter a valid title"
-            value={props.placeToBeUpdated.title}
-            valid={true}
+            value={formState.inputs.title.value}
+            valid={formState.inputs.title.isValid}
+            onInput={inputHandler}
             />
             
             <Input id="description"
@@ -30,11 +49,12 @@ export default function UpdatePlace(props){
             label="Description"
             validators={[VALIDATOR_MINLENGTH(10)]}
             errorText="Decription should be of atleast 10 characters"
-            value={props.placeToBeUpdated.description}
-            valid={true}
+            value={formState.inputs.description.value}
+            valid={formState.inputs.title.isValid}
+            onInput={inputHandler}
             />
 
-            <Button disabled={true}>Update place</Button>
+            <Button type="submit" disabled={!formState.isValid}>Update place</Button>
         </form>
     )
 }
