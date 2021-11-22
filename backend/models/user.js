8ed6487e8fb9e37b5cf5ef6e85cac08fb0df98be
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-11-22 16:43:53 +0530

    Update schema to have one to many relation

---
+++ backend/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const uniqueValidator = require('mongoose-unique-validator')
+const userSchema = mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true, unique:true},
+    password:{type:String,required:true, minlength:7},
+    image:{type:String, required:true},
+    places:[{type:mongoose.Types.ObjectId,required:true,ref:'Place'}]
+})
+
+userSchema.plugin(uniqueValidator)
+const UserModel = mongoose.model('User',userSchema)
+
+module.exports = UserModel
